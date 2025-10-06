7d32dc19e6598ec38cdd39bf6194cbe27f995902
mazharinandrei <79073599+mazharinandrei@users.noreply.github.com>
2025-10-06 16:29:02 +0700

    refactor: перенёс ObjectPermissionRequiredMixin

--- project/main/mixins/object_permission_required.py
+++ project/main/mixins/object_permission_required.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 
+
 class ObjectPermissionRequiredMixin(LoginRequiredMixin):
     """
     Сравнивает object.created_by и request.user.
     PermissionDenied, eсли объект создан другим пользователем
     """
+
     def dispatch(self, request, *args, **kwargs):
         if not self.get_object().created_by == request.user:
             raise PermissionDenied()
